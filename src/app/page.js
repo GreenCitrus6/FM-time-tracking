1860e6177867894b2c8c198b6c702f2a0c1494af
Daniel A <DanielAAad01@gmail.com>
2023-12-18 02:25:46 -0800

    Added link to portfolio website to #attribution and README.md

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
   
       <div id="attribution" className="mt-2 flex justify-center italic text-gray-400">
         <p className="mr-2">Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-neutral-paleBlue active:text-white">Frontend Mentor</a>.</p>
-        <p>Coded by <a href="#" className="text-neutral-paleBlue active:text-white">Daniel Aadland</a>. </p>
+        <p>Coded by <a href="https://portfolio-website-rgm2jbtrb-greencitrus6s-projects.vercel.app/" className="text-neutral-paleBlue active:text-white">Daniel Aadland</a>. </p>
       </div>
     </main>
   )
